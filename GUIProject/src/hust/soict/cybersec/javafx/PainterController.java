ceb7eb7394bbb2ca497ebd3490bfc9f1d1e0ce26
exomuck <tranhuy.dev88@gmail.com>
2024-05-28 23:01:19 +0700

    update lab05 - done GUIProject

--- GUIProject/src/hust/soict/cybersec/javafx/PainterController.java
+++ GUIProject/src/hust/soict/cybersec/javafx/PainterController.java
@@ -2,6 +2,8 @@ package hust.soict.cybersec.javafx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -11,9 +13,32 @@ public class PainterController {
 
     @FXML
     private Pane drawingAreaPane;
+    
+    @FXML
+    private ToggleGroup color;
+    
+    @FXML
+    private RadioButton pen;
+    
+    @FXML
+    private RadioButton eraser;
 
     @FXML
     void drawingAreaMouseDragged(MouseEvent event) {
+    	Color selectedColor = Color.BLACK;
+    	
+    	if (pen.isSelected()) {
+            selectedColor = Color.BLACK;
+        } else if (eraser.isSelected()) {
+            selectedColor = Color.WHITE;
+        }
+    	
+    	Circle newCircle = new Circle(event.getX(), event.getY(), 4, selectedColor);
+    	drawingAreaPane.getChildren().add(newCircle);
+    }
+    
+    @FXML
+    void drawingAreaMousePressed(MouseEvent event) {
     	Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
     	drawingAreaPane.getChildren().add(newCircle);
     }
